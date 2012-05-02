83b22b3afe1bf01c8b85b13845f245c777028ecd
Telerim <info@telerim.com>
2012-05-02 02:47:19 +0000

    Changed "New User" link to link to plugin

--- Cake/View/Helper/FormHelper.php
+++ Cake/View/Helper/FormHelper.php
@@ -990,10 +990,15 @@ class FormHelper extends AppHelper {
                                 // Adds a "New" link to every related select
                                 $varField = Inflector::tableize(Inflector::pluralize(preg_replace('/_id$/', '', $fieldKey)));
                                 $varName = Inflector::humanize(Inflector::singularize(preg_replace('/_id$/', '', $fieldKey)));
+				$optPlugin = "";
+				if ($varname === "User") 
+					$optPlugin = "Usermgmt";
+				$optPlugin = 
                                 $options['after'] = $this->Html->link(
                                         __('New') . " " . $varName,
                                         array('controller' => $varField,
-                                                'action' => 'add'),
+                                                'action' => 'add',
+						'plugin' => $optPlugin),
                                         array('target' => '_blank')
                                 );
                                 /* CRM core hack - end */
